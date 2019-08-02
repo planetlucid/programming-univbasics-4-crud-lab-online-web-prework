14ff86094fa0511dda016dfc6d7ea71cd90bd591
restful-strategy-4685 <planetlucid@gmail.com>
2019-08-02 21:48:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array  
-  ["lil' bub", "grumpy cat", "Maru"]
+  ["lil' bub", "grumpy cat", "Maru", "Jake"]
 end
 
 def add_element_to_end_of_array(array, element)   
