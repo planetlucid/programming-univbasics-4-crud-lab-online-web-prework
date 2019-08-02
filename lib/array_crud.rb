527c272137d145f26eeeeeb2158f4d071996bb1c
restful-strategy-4685 <planetlucid@gmail.com>
2019-08-02 20:49:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,16 +11,16 @@ end
 
 def add_element_to_start_of_array(array, element)
    = [1,2,3,4]
-  add_element_to_end_of_array.unshift("nala cat")
+  add_element_to_start_of_array.unshift("nala cat")
 end
 
 def remove_element_from_end_of_array(array)
    = [1,2,3,4]
-  add_element_to_end_of_array.pop[-1]
+  remove_element_from_end_of_array(array).pop[-1]
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_end_of_array(array).pop[-1]
 end
 
 def retrieve_element_from_index(array, index_number)
