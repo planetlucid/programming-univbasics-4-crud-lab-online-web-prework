f2932514f3bee5448db2b2732a5ce5552ce586bc
restful-strategy-4685 <planetlucid@gmail.com>
2019-08-02 21:49:36 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
-def create_an_empty_array
-  
+def create_an_empty_array 
+  []
 end
 
-def create_an_array
-  
+def create_an_array  
+  ["lil' bub", "grumpy cat", "Maru", "Jake"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array, element)   
+    array << element
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(array, element)  
+  array.unshift(element)
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(array) 
+ array.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(array) 
+ array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(array, index_number) 
+  array[index_number]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(array) 
+  array[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(array) 
+  array[-1]
 end
 
-def update_element_from_index(array, index_number, element)
-
+def update_element_from_index(array, index_number, element) 
+array[index_number] = element
 end
