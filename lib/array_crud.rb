8348e91f8680fd78b66594461bf29555e39176dc
restful-strategy-4685 <planetlucid@gmail.com>
2019-08-02 20:39:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+   = [1,2,3,4]
+  add_element_to_end_of_array.pop"nala cat"
 end
 
 def remove_element_from_start_of_array(array)
