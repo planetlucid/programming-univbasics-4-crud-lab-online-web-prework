06dd81fb879b1c74440c5c3c946be60609b85574
restful-strategy-4685 <planetlucid@gmail.com>
2019-08-02 21:11:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,37 +2,37 @@ def create_an_empty_array
   []
 end
 
-def create_an_array = ["lil' bub", "grumpy cat", "Maru"]
+def create_an_array  ["lil' bub", "grumpy cat", "Maru"]
 end
 
-def add_element_to_end_of_array(array, element)  = ["lil' bub", "grumpy cat", "Maru"]
+def add_element_to_end_of_array(array, element)   ["lil' bub", "grumpy cat", "Maru"]
   add_element_to_end_of_array << "nala cat"
 end
 
-def add_element_to_start_of_array(array, element) = ["lil' bub", "grumpy cat", "Maru"]
+def add_element_to_start_of_array(array, element)  ["lil' bub", "grumpy cat", "Maru"]
   add_element_to_start_of_array.unshift("nala cat")
 end
 
-def remove_element_from_end_of_array(array)   = ["lil' bub", "grumpy cat", "Maru"]
+def remove_element_from_end_of_array(array) ["lil' bub", "grumpy cat", "Maru"]
   remove_element_from_end_of_array(array).pop[-1]
 end
 
-def remove_element_from_start_of_array(array)  = ["lil' bub", "grumpy cat", "Maru"]
+def remove_element_from_start_of_array(array) ["lil' bub", "grumpy cat", "Maru"]
   lil_bub = remove_element_from_start_of_array.shift
 end
 
-def retrieve_element_from_index(array, index_number) = ["lil' bub", "grumpy cat", "Maru"]
+def retrieve_element_from_index(array, index_number) ["lil' bub", "grumpy cat", "Maru"]
   retrieve_element_from_index[0]
 end
 
-def retrieve_first_element_from_array(array) = ["lil' bub", "grumpy cat", "Maru"]
+def retrieve_first_element_from_array(array) ["lil' bub", "grumpy cat", "Maru"]
   retrieve_first_element_from_array[0]
 end
 
-def retrieve_last_element_from_array(array) = ["lil' bub", "grumpy cat", "Maru"]
+def retrieve_last_element_from_array(array) ["lil' bub", "grumpy cat", "Maru"]
   retrieve_last_element_from_array[-1]
 end
 
-def update_element_from_index(array, index_number, element) = ["lil' bub", "grumpy cat", "Maru"]
+def update_element_from_index(array, index_number, element) ["lil' bub", "grumpy cat", "Maru"]
 update_element_from_index[2] => "something else"
 end
