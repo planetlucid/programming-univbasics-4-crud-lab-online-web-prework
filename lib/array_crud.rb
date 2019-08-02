2e06ab907eaa0248cba2242c0db85a808b1f7e4c
restful-strategy-4685 <planetlucid@gmail.com>
2019-08-02 20:56:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,13 +25,13 @@ def retrieve_element_from_index(array, index_number) = ["lil' bub", "grumpy cat"
 end
 
 def retrieve_first_element_from_array(array) = ["lil' bub", "grumpy cat", "Maru"]
-  
+  retrieve_first_element_from_array[0]
 end
 
 def retrieve_last_element_from_array(array) = ["lil' bub", "grumpy cat", "Maru"]
-  
+  retrieve_last_element_from_array[-1]
 end
 
 def update_element_from_index(array, index_number, element) = ["lil' bub", "grumpy cat", "Maru"]
-
+update_element_from_index [2] => "something else"
 end
